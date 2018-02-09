bc29e4db3910b325ef274317f42b2a407cec790d
David <hanwei@yunwii.com>
2018-02-09 20:29:51 +0800

    添加牛人列表接口

--- user.js
+++ user.js
@@ -7,8 +7,9 @@ const _filter = { pwd: 0, __v: 0 };
 
 Router.get('/list', (req, res) => {
   // User.remove({}, (req, res) => {});
-  User.find({}, (err, doc) => {
-    return res.json(doc);
+  const { type } = req.query;
+  User.find({ type }, (err, doc) => {
+    return res.json({code: 0, data: doc});
   });
 });
 
@@ -21,6 +22,26 @@ Router.post('/login', (req, res) => {
     }
     res.cookie('userid', doc._id);
     return res.json({ code: 0, data: doc });
+  });
+});
+
+// 更新资料
+Router.post('/update', (req, res) => {
+  const userid = req.cookies.userid;
+  if (!userid) {
+    return json.dumps({ code: 1 });
+  }
+  const body = req.body;
+  User.findByIdAndUpdate(userid, body, (err, doc) => {
+    const data = Object.assign(
+      {},
+      {
+        user: doc.user,
+        type: doc.type
+      },
+      body
+    );
+    return res.json({ code: 0, data });
   });
 });
 
