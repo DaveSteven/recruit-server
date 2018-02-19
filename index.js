403a5a31a62cee457e2421051311f6da8c740ba4
David <hanwei@yunwii.com>
2018-02-19 14:34:11 +0800

    添加socket服务

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const cookieParser = require('cookie-parser');
 // 新建app
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  console.log('user login');
+  socket.on('send-msg', data => {
+    io.emit('receive', data);
+  });
+});
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use('/user', userRouter);
 
-app.listen(9099, () => {
+server.listen(9099, () => {
   console.log('Node app start at port 9099');
 });
